18913fab88750bd35315b1f52f1391d2e0a8bef5
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-29 12:22:08 +0100

    Update BombHasBeenPlanted.java

--- BombHasBeenPlanted.java
+++ BombHasBeenPlanted.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-class TestBomb {
+class BombHasBeenPlanted {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
