c96b83d4b6c36b0955286ce7eca4412976b18bfc
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-30 12:43:58 +0300

    Create MatchFullName.java

---
+++ MatchFullName.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName {
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String regex = "\\b(?<firstName>[A-Z][a-z]+) (?<lastName>[A-Z][a-z]+)\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+            String currentName = matcher.group() + " ";
+            System.out.print(currentName);
+        }
+    }
+}
